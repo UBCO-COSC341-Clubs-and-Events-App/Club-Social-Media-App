9e7c77d2a5970ea2a5e6d4fdef863092be5ceda0
HakimRashid <rashidhakim514@gmail.com>
2025-12-04 23:07:13 -0800

    Added a log out function fixed imports

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
